0f9501c1195b844a2b01433e57e4d27b2987da4f
fanrui <fanrui@weli.cn>
2018-08-22 16:20:18 +0800

    azkaban检查调度中，strategy变量更新为disabledStrategy变量，用来决定job的禁用策略

--- venv/Scripts/azkaban_check_api.py
+++ venv/Scripts/azkaban_check_api.py
@@ -10,10 +10,10 @@ import logging
 import urllib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-# 策略选择，可选择的策略有：depend, direct 。
+# 禁用策略选择(此api进行的调度会禁用一些之前已经执行成功的job)，可选择的策略有：depend, direct 。
 # direct 表示将状态为 SUCCEEDED 或者 SKIPPED 的job做disabled操作
 # depend 表示 不仅将状态为 SUCCEEDED 或者 SKIPPED 的job做disabled操作，而且将这些job依赖的job也做 disabled操作
-strategy = 'depend' # 可选择的策略有：depend, direct
+disabledStrategy = 'depend' # 可选择的策略有：depend, direct
 
 # 禁用安全请求警告
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -97,7 +97,7 @@ def recursionAddDependence(succeededJobs, dependenceJobs, jobName):
 def addJob(r):
     global retryFlows
     succeededJobs = []
-    if strategy == 'depend':
+    if disabledStrategy == 'depend':
         dependenceJobs = {}
         for job in r['nodes']:  # 遍历所有job，构建所有job之间的依赖关系
             if 'in' in job:   # 这个job有依赖的job
@@ -105,9 +105,9 @@ def addJob(r):
 
     for job in r['nodes']:  # 遍历所有job，将成功的job添加到succeededJobs中
         if job['status'] == 'SUCCEEDED' or job['status'] == 'SKIPPED':  # 此job已经执行成功了，把该job和该job的所有依赖 加入到 succeededJobs
-            if strategy == 'depend':
+            if disabledStrategy == 'depend':
                 recursionAddDependence(succeededJobs, dependenceJobs, job['id'])
-            elif strategy == 'direct':
+            elif disabledStrategy == 'direct':
                 succeededJobs.append(job['id'])
     retryFlows.append(ExecuteFlow(project=r['project'], flow=r['flow'], disabled=succeededJobs))
     logging.info(r['project'] + u'项目的Flow：' + r['flow']
