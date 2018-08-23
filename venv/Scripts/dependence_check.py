f9cdea49c9ea647ca4154025d77e93e663a7e13f
fanrui <fanrui@weli.cn>
2018-08-23 17:33:51 +0800

    修复了 多次等待会造成 项目变多的bug

--- venv/Scripts/dependence_check.py
+++ venv/Scripts/dependence_check.py
@@ -10,10 +10,6 @@ import logging
 import urllib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-# 禁用策略选择(此api进行的调度会禁用一些之前已经执行成功的job)，可选择的策略有：depend, direct 。
-# direct 表示将状态为 SUCCEEDED 或者 SKIPPED 的job做disabled操作
-# depend 表示 不仅将状态为 SUCCEEDED 或者 SKIPPED 的job做disabled操作，而且将这些job依赖的job也做 disabled操作
-disabledStrategy = 'depend' # 可选择的策略有：depend, direct
 
 # 禁用安全请求警告
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -23,7 +19,7 @@ username = 'fanrui'
 password = '123123'
 failureEmails = ''
 session_id = ''
-dependenceProjectsName = [u'wnl_user_daily_new_active_stats',u'wnl_custom_event_stats',u'wnl_pv_event_stats']    # u'wnl_test',u'wnl_user_daily_new_active_stats',u'wnl_custom_event_stats',u'wnl_pv_event_stats'
+dependenceProjectsName = [u'wnl_sqoop_syc_stats',u'wnl_user_daily_new_active_stats',u'wnl_pv_event_stats']    # u'wnl_sqoop_syc_stats',u'wnl_user_daily_new_active_stats',u'wnl_custom_event_stats',u'wnl_pv_event_stats'
 dependenceAzkabanProjects = []
 
 class AzkabanProject:
@@ -34,15 +30,6 @@ class AzkabanProject:
         self.projectId = projectId
         self.flows = flows
 
-
-class ExecuteFlow:
-    '要执行的flow的信息'
-
-    def __init__(self, project, flow, disabled):
-        self.project = project
-        self.flow = flow
-        self.disabled = '[\"' + '\",\"'.join(disabled) + '\"]'
-        # self.disabled = disabled
 
 def consoleOut():
     ''''' Output log to file and console '''
@@ -72,6 +59,7 @@ def login():
 
 # 根据依赖项目名 获取依赖项目的所有 flow,将获取的对象 加到 dependenceAzkabanProjects 中
 def fetchFlowsOfProject():
+    global dependenceAzkabanProjects
     for projectName in dependenceProjectsName:
         param = {'ajax': 'fetchprojectflows', 'session.id': session_id, 'project': projectName}
         r = requests.get(url="https://" + azkabanIP + ":" + azkabanPort + "/manager", verify=False, params=param).json()
@@ -111,16 +99,18 @@ def fetchFlowExecutions():
                         if job['id'] == flow and job['status'] != 'SUCCEEDED':  # 依赖项目的最终job失败
                             logging.info(u'依赖的' + dependenceAzkabanProject.name + u'项目的Flow：' + flow + u'，最终job的状态为' + job['status'] + u'，所以该任务需要等待')
                             return False
+                    logging.info(u'依赖的' + dependenceAzkabanProject.name + u'项目的Flow：' + flow
+                                 + u'执行id为' + str(execid) + u'，该flow最后job成功，检查通过')
                 else:
                     logging.info(u'依赖的' + dependenceAzkabanProject.name + u'项目的Flow：' + flow + u'，当前运行状态为：' + flowStatus + u'，所以该任务需要等待')
                     return False
             else:
                 logging.info(u'依赖的' + dependenceAzkabanProject.name + u'项目的Flow：' + flow + u'今天还未进行调度，所以该任务需要等待')
                 return False
+    logging.info(u'所有依赖项目检查完毕，全部通过')
     return True
 
 
-# 使用 crontab 进行定时调度
 if __name__ == '__main__':
     consoleOut()
     flag = False
@@ -128,15 +118,16 @@ if __name__ == '__main__':
     while not flag:
         if count > 0:
             if count > 6:
-                logging.info('检查' + count + '次后，依赖仍然没有执行成功，不再等待，直接挂掉')
+                logging.info('检查' + str(count) + '次后，依赖仍然没有执行成功，不再等待，直接挂掉')
                 sys.exit(1)
-            logging.info('第' + count + '次等待中。。。')
+            logging.info('第' + str(count) + '次等待中。。。')
             time.sleep(600.0)
         count = count + 1
 
         login()                 # 登录账号,获取 session_id
         fetchFlowsOfProject()   # 根据项目名 获取项目对象
-        flat = fetchFlowExecutions()   # 根据项目对象 构建需要重试的 retryFlows 列表
+        flag = fetchFlowExecutions()   # 所有依赖都完成返回True，否则返回False
+        dependenceAzkabanProjects = []
 
-    logging.info('检查' + count + '次后，依赖执行成功，check通过')
+    logging.info('检查' + str(count) + '次后，所有依赖项目都执行成功，check通过')
     sys.exit(0)
